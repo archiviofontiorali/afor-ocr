59e57eace61fbf391a754075240248971b511a15
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2020-11-29 22:26:01 +0100

    Move code for creating sample image to demo package

--- demo/images.py
+++ demo/images.py
@@ -1,5 +1,8 @@
 import pathlib
-from typing import Tuple, List
+from typing import List, Tuple
+
+import cv2
+import numpy as np
 
 import ocr.io
 
@@ -7,6 +10,7 @@ from .types import Image, Path
 
 
 def load_from_folder(path: Path) -> List[Tuple[str, Image]]:
+    """Load all images from a specific folder."""
     images = []
     for ext in ("png", "jpg"):
         for image_path in sorted(pathlib.Path(path).glob(fr"*.{ext}")):
@@ -15,5 +19,22 @@ def load_from_folder(path: Path) -> List[Tuple[str, Image]]:
     return images
 
 
-def create_sample_image() -> Tuple[str, Image]:
-    return "opencv", ocr.io.create_example_image((256, 256), text="OpenCV")
+def create_sample_image(width=256, height=None, text="opencv") -> Tuple[str, Image]:
+    """Generate a simple gradient image with some optional text."""
+    if height is None:
+        height = width
+    image = np.empty((height, width, 3), dtype=np.uint8)
+
+    colors = (
+        np.array([255, 0, 0], dtype=np.uint8),
+        np.array([0, 255, 0], dtype=np.uint8),
+    )
+    for j in range(height):
+        image[j, ...] = (j / 255) * colors[0] + (1 - j / 255) * colors[1]
+
+    if text:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        color = (255, 255, 255)
+        cv2.putText(image, text, (10, height // 2), font, 2, color, 2, cv2.LINE_AA)
+
+    return "opencv", image
--- ocr/io.py
+++ ocr/io.py
@@ -25,23 +25,3 @@ def load_image(path: Union[str, Path]) -> np.ndarray:
         raise FileNotFoundError(path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
-
-
-def create_example_image(shape=(256, 256), text: str = None):
-    """Generate a simple gradient image with some optional text."""
-    width, height = shape
-    image = np.empty((height, width, 3), dtype=np.uint8)
-
-    colors = (
-        np.array([255, 0, 0], dtype=np.uint8),
-        np.array([0, 255, 0], dtype=np.uint8),
-    )
-    for j in range(height):
-        image[j, ...] = (j / 255) * colors[0] + (1 - j / 255) * colors[1]
-
-    if text:
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        color = (255, 255, 255)
-        cv2.putText(image, text, (10, height // 2), font, 2, color, 2, cv2.LINE_AA)
-
-    return image
