0e3119c23f5133660821c33328e6c7bbb34af0c7
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2020-11-30 01:11:46 +0100

    Improve Tesseract class with delegates, default values and better detection output

--- ocr/tesseract.py
+++ ocr/tesseract.py
@@ -1,16 +1,45 @@
+from typing import List
+
 import pytesseract
+
+from ocr.types import delegates
+
+
+class Word:
+    def __init__(self, x0, y0, w, h, confidence, text: str):
+        self.x0, self.y0 = int(x0), int(y0)
+        self.w, self.h = int(w), int(h)
+        self.confidence = confidence
+        self.text = text
+
+        self.x1 = self.x0 + self.w
+        self.y1 = self.y0 + self.h
+
+    def __repr__(self):
+        return (
+            f"Word(({self.x0}, {self.y0}, {self.x1}, {self.y1}), "
+            f"confidence={self.confidence}, text={self.text})"
+        )
 
 
 class Tesseract:
     def __init__(self, lang: str = "eng"):
-        self.config = dict(
-            lang=lang,
-            # config="",
-            # nice=0,
-            # output_type=Output.STRING,
-            # timeout=0,
-            # pandas_config=None,
-        )
+        self.lang = lang
 
-    def extract(self, image) -> str:
-        return pytesseract.image_to_string(image, **self.config)
+    @delegates(pytesseract.image_to_string)
+    def extract(self, image, **kwargs) -> str:
+        kwargs.setdefault("lang", self.lang)
+        return pytesseract.image_to_string(image, **kwargs)
+
+    @delegates(pytesseract.image_to_data)
+    def detect(self, image, **kwargs) -> List[Word]:
+        kwargs.setdefault("lang", self.lang)
+        data: str = pytesseract.image_to_data(image, **kwargs)
+        header, *rows = data.strip().split("\n")
+
+        words = []
+        for row in rows:
+            x0, y0, w, h, confidence, text = row.split("\t")[-6:]
+            words.append(Word(x0, y0, w, h, confidence, text))
+
+        return words
